97381463e117d71eb7f61083b78741dfeae1c138
soundsgoodgames <goodsound06@gmail.com>
2024-02-17 14:13:59 +0900

    backend address

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
 import {io} from "socket.io-client";
-const socket = io("http://localhost:5001");
+const socket = io("https://port-0-mbti-talk-backend-dc9c2nlspm6yjd.sel5.cloudtype.app/");
+//const socket = io("http://localhost:5001");
 export default socket;
